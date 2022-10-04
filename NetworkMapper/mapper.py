e637d6bd5cb9a92aa8f12610110233095af5a185
saharshbhansali <saharsh.bhansali@gmail.com>
2022-10-05 05:04:49 +0530

    (update): changed mapper to functional paradigm

--- NetworkMapper/mapper.py
+++ NetworkMapper/mapper.py
@@ -1,18 +1,21 @@
 from socket import *
 import time
-startTime = time.time()
 
-if __name__ == '__main__':
+def portScan():
+   startTime = time.time()
+
    target = input('Enter the host to be scanned: ')
    t_IP = gethostbyname(target)
+   start, end = eval(input("Enter port range( start, stop): "))
    print ('Starting scan on host: ', t_IP)
-   
-   for i in range(50, 500):
+
+   for i in range(start, end+1):
       s = socket(AF_INET, SOCK_STREAM)
       
       conn = s.connect_ex((t_IP, i))
       if(conn == 0) :
          print ('Port %d: OPEN' % (i,))
       s.close()
-print('Time taken:', time.time() - startTime)
+   print('Time taken:', time.time() - startTime)
 
+portScan()
