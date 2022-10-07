30cf1d8dd16f55e5fed39b6499b7587775a4196b
saharshbhansali <saharsh.bhansali@gmail.com>
2022-10-07 22:02:06 +0530

    (update) made template structure for mapper.py (add) added template for a BurpIntruder Clone

---
+++ Networks/BurpIntruder/intruder.py
@@ -0,0 +1,18 @@
+from pycurl import *
+from socket import *
+from scapy import *
+
+def basicIntruder():
+    pass
+
+def disassembleRequest():
+    pass
+
+def modifyRequest():
+    pass
+
+def sendRequest():
+    pass
+
+if __name__ == "__main__":
+    pass
